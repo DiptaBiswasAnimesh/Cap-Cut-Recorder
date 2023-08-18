375a57abad8b55952f7ae2eb2983d85d2a5b68af
unknown <diptabiswas223@gmail.com>
2023-08-19 04:23:26 +0600

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿
+namespace Cap_Cut_Recorder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+
+        }
+
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblRecordingStatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.button2.BackgroundImage = global::Cap_Cut_Recorder.Properties.Resources.THH_25121916_25131916_25126916_25136916_001;
+            this.button2.Image = global::Cap_Cut_Recorder.Properties.Resources._20230819_032959;
+            this.button2.Location = new System.Drawing.Point(394, 78);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(105, 105);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = global::Cap_Cut_Recorder.Properties.Resources.THH_25121916_25131916_25126916_25136916_001;
+            this.button1.Image = global::Cap_Cut_Recorder.Properties.Resources._20230819_032727;
+            this.button1.Location = new System.Drawing.Point(171, 78);
+            this.button1.Margin = new System.Windows.Forms.Padding(0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 105);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblRecordingStatus
+            // 
+            this.lblRecordingStatus.AutoSize = true;
+            this.lblRecordingStatus.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.lblRecordingStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRecordingStatus.ForeColor = System.Drawing.Color.White;
+            this.lblRecordingStatus.Location = new System.Drawing.Point(243, 218);
+            this.lblRecordingStatus.Name = "lblRecordingStatus";
+            this.lblRecordingStatus.Size = new System.Drawing.Size(0, 20);
+            this.lblRecordingStatus.TabIndex = 2;
+            this.lblRecordingStatus.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.BackgroundImage = global::Cap_Cut_Recorder.Properties.Resources.THH_25121916_25131916_25126916_25136916_001;
+            this.ClientSize = new System.Drawing.Size(665, 261);
+            this.Controls.Add(this.lblRecordingStatus);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label lblRecordingStatus;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+using AForge.Video;
+using AForge.Video.FFMPEG;
+using NAudio.Wave;
+
+namespace Cap_Cut_Recorder
+{
+    public partial class Form1 : Form
+    {
+        private int videoCount = 0; // To track the number of recorded videos
+        private VideoFileWriter writer;
+        private ScreenCaptureStream screenCapture;
+        private WaveInEvent waveIn;
+        private MemoryStream audioStream;
+        private bool isRecording = false;
+        private string audioOutputPath;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!isRecording)
+            {
+                StartRecording();
+            }
+            else
+            {
+                StopRecording();
+            }
+        }
+
+        private void StartRecording()
+        {
+            videoCount++; // Increment video count
+
+            string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string videoFileName = $"output{videoCount}.mp4"; // Use incremental file name for video
+            string videoOutputPath = Path.Combine(desktopPath, videoFileName);
+            string audioTempPath = Path.Combine(desktopPath, $"temp{videoCount}.aac"); // Temporary audio path
+
+            Rectangle screenBounds = Screen.PrimaryScreen.Bounds;
+
+            writer = new VideoFileWriter();
+            writer.Open(videoOutputPath, screenBounds.Width, screenBounds.Height, 15, VideoCodec.MPEG4);
+
+            screenCapture = new ScreenCaptureStream(screenBounds);
+            screenCapture.NewFrame += ScreenCapture_NewFrame;
+            screenCapture.Start();
+
+            waveIn = new WaveInEvent();
+            waveIn.DataAvailable += WaveIn_DataAvailable;
+            waveIn.WaveFormat = new WaveFormat(44100, 1); // Adjust audio settings (44100 Hz, mono)
+            audioStream = new MemoryStream();
+            waveIn.StartRecording();
+
+            audioOutputPath = audioTempPath;
+
+            isRecording = true;
+            button1.Text = "";
+            lblRecordingStatus.Text = "Recording In Progress";
+        }
+
+        private void StopRecording()
+        {
+            if (isRecording)
+            {
+                screenCapture.SignalToStop();
+                screenCapture.WaitForStop();
+                writer.Close();
+
+                waveIn.DataAvailable -= WaveIn_DataAvailable;
+                waveIn.StopRecording();
+                waveIn.Dispose();
+
+                audioStream.Seek(0, SeekOrigin.Begin);
+
+                using (FileStream fs = new FileStream(audioOutputPath, FileMode.Create))
+                {
+                    audioStream.CopyTo(fs);
+                }
+
+                audioStream.Dispose();
+
+                // Use FFmpeg to merge audio and video
+                string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+                string videoFileName = $"output{videoCount}.mp4";
+                string audioFileName = $"temp{videoCount}.aac";
+                string mergedFileName = $"merged{videoCount}.mp4";
+                string mergedOutputPath = Path.Combine(desktopPath, mergedFileName);
+
+                string ffmpegArgs = $"-i \"{videoFileName}\" -i \"{audioFileName}\" -c:v copy -c:a aac \"{mergedOutputPath}\"";
+                RunFFmpegProcess(ffmpegArgs);
+
+                // Delete temporary audio file
+                File.Delete(audioOutputPath);
+
+                isRecording = false;
+                button1.Text = "";
+                lblRecordingStatus.Text = "";
+            }
+        }
+        private void RunFFmpegProcess(string arguments)
+        {
+            using (System.Diagnostics.Process process = new System.Diagnostics.Process())
+            {
+                process.StartInfo.FileName = "ffmpeg.exe";
+                process.StartInfo.Arguments = arguments;
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.CreateNoWindow = true;
+                process.StartInfo.RedirectStandardOutput = true;
+                process.StartInfo.RedirectStandardError = true;
+                process.Start();
+                process.WaitForExit();
+            }
+        }
+
+        private void ScreenCapture_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            writer.WriteVideoFrame(eventArgs.Frame);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            CaptureScreenshot();
+        }
+        private void WaveIn_DataAvailable(object sender, WaveInEventArgs eventArgs)
+        {
+            audioStream.Write(eventArgs.Buffer, 0, eventArgs.BytesRecorded);
+        }
+        private void CaptureScreenshot()
+        {
+            try
+            {
+                string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+                string fileName = $"Screenshot_{DateTime.Now:yyyyMMddHHmmss}.png";
+                string filePath = System.IO.Path.Combine(desktopPath, fileName);
+
+                using (Bitmap screenshot = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height))
+                {
+                    using (Graphics graphics = Graphics.FromImage(screenshot))
+                    {
+                        graphics.CopyFromScreen(0, 0, 0, 0, screenshot.Size);
+                    }
+
+                    screenshot.Save(filePath, System.Drawing.Imaging.ImageFormat.Png);
+                }
+
+                MessageBox.Show($"Screenshot saved to:\n{filePath}", "Screenshot Captured", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error capturing screenshot: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
